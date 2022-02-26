82c3e7cc353ec210b3c39e1b7b13c11786fd3527
Gerardo <Villalobos>
2022-02-26 09:47:24 -0600

    .

---
+++ TestConsoleDuende/TokenHelper.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleDuende
+{
+    public static class TokenHelper
+    {
+        const string _clientId = "mtwdm2022";
+        const string _clientSecret = "superpassword";
+        const string BaseAddress = "https://localhost:7030";
+
+        internal static AuthToken GetAccessToken()
+        {
+            var fields = new Dictionary<string, string>
+            {
+                {"scope","ApiWebDuende" },
+                {"grant_type","client_credentials" },
+            };
+            string creds = String.Format("{0}:{1}", _clientId, _clientSecret);
+            byte[] bytes = Encoding.ASCII.GetBytes(creds);
+            var header = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(bytes));
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = header;
+            client.BaseAddress = new Uri(BaseAddress + "/connect/token");
+
+            var response = client.PostAsync(String.Empty, new FormUrlEncodedContent(fields)).Result;
+            string raw = response.Content.ReadAsStringAsync().Result;
+            var json = JObject.Parse(raw);
+            JToken token;
+            if (json.TryGetValue("access_token", out token))
+                return JsonConvert.DeserializeObject<AuthToken>(json.ToString());
+            
+            return null;
+        }
+    }
+}
---
+++ TestConsoleDuende/WebProxyHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleDuende
+{
+    public static class WebProxyHelper
+    {
+        public static Boolean GetAuthenticateResponse()
+        {
+            var client = new HttpClient();
+            var token = TokenHelper.GetAccessToken();
+            client.DefaultRequestHeaders.Authorization= new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer",token.AccessToken);
+            return true;
+        }
+    }
+}
